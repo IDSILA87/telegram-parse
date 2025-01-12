27f2889ad137fb48d6d03ea1865b83e2a3c665a2
IDSILA86 <idsila87@gmail.com>
2025-01-12 20:03:15 +0400

    update

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const axios = require('axios');
+const cheerio = require("cheerio");
+
+const app = express();
+
+app.use(cors({methods:['POST','GET']}));
+app.use(express.json());
+
+let size = 0;
+const url = 'https://telegram-parse.glitch.me';
+const symbols = '0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm-_';
+const maska = 'XXXXXXXXXXXXXXXX'.split('');
+const generateChannel = () => {
+  return maska.reduce((acc) =>  acc+symbols.split('')[Math.ceil(Math.random()*symbols.length)-1], '')
+}
+
+
+
+const startApp = () => {
+  const hash = generateChannel();   
+  size += 1;
+  axios(`https://t.me/+${hash}`).then(res =>{
+    const html = cheerio.load(res.data);
+    const name = html('span[dir="auto"]')[0];
+    if(name != undefined){
+      axios(`${url}/add?hash=${hash}`).then( res_2 => {
+        startApp();
+      })    
+   }
+   else{
+     startApp();
+   }        
+ })
+}
+
+startApp();
+
+      
+app.get('/sleep',(req, res) =>{
+  res.send({type: true});
+});
+
+app.post('/sleep',(req, res) =>{
+  res.send({type: true});
+});
+
+
+setInterval(() => {
+  axios(`${url}/update?size=${size}`).then(res_2 => {
+    size = 0;
+  })
+},3*60000);
+
+const listener = app.listen('3000', () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
